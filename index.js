8b4b08007270c2e0a7ec4078c5f04ab0228ba71c
Gaspar Errobidart <podmaster@hotmail.com>
2020-05-28 16:47:20 -0300

    Update docker and chrome config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
-  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags , port : process.env.PORT}).then(chrome => {
     opts.port = chrome.port;
     return lighthouse(url, opts, config).then(results => {
       // use results.lhr for the JS-consumable output
@@ -10,12 +10,12 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
       // use results.report for the HTML/JSON/CSV output as a string
       // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
       return chrome.kill().then(() => results.lhr)
-    });
-  });
+    }).catch( err=> console.error(err));
+  }).catch( err=> console.error(err));
 }
 
 const opts = {
-  chromeFlags: ['--show-paint-rects']
+  chromeFlags: ['--headless','--remote-debugging-address=0.0.0.0', '--no-sandbox']
 };
 
 // Usage:
